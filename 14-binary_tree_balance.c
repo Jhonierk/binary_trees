a763279a14156b3daac25e832fa95fe0c13d0a3a
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-11-25 19:29:40 -0500

    add function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,3 +17,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left - right);
 }
+
+/**
+ * aux_binary_tree_height - Measure the height
+ * of a binary tree from a given node
+ * @tree: pointer to node of tree to measure
+ * Description: Edited to work with balance factor function
+ * Return: height of tree or -1 if NULL
+ */
+int aux_binary_tree_height(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (-1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left = custom_binary_tree_height(tree->left) + 1;
+	right = custom_binary_tree_height(tree->right) + 1;
+
+	if (left > right)
+		return (left);
+	else
+		return (right);
+}
